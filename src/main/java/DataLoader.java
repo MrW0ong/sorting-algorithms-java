6335814700da72a0ac0d15b7ffa9cde8910cc405
MrW0ong <80582180+MrW0ong@users.noreply.github.com>
2025-11-10 19:15:51 +0100

    Added code to parse .csv files

--- src/main/java/DataLoader.java
+++ src/main/java/DataLoader.java
@@ -1,2 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
 public class DataLoader {
+    public List<Double> loadData(String filePath) throws FileNotFoundException {
+        List<Double> alcohol = new ArrayList<>();
+
+        try (Scanner sc = new Scanner(new File(filePath))) {
+
+            if (sc.hasNextLine()) sc.nextLine(); // skip header
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.isBlank()) continue;
+
+                String[] values = line.split(";");
+                String s = values[10].trim();
+                if (s.isEmpty()) continue;
+
+                double alc = Double.parseDouble(s);
+                alcohol.add(alc);
+
+            }
+        } return alcohol;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,32 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
-}
+    public static void main(String[] args) throws FileNotFoundException {
+        DataLoader loader = new DataLoader();
+
+        // Load CSV files
+        List<Double> redWine = loader.loadData("src/main/resources/winequality-red.csv");
+        List<Double> whiteWine = loader.loadData("src/main/resources/winequality-white.csv");
+
+        System.out.println("Red rows: " + redWine.size());
+        System.out.println("White rows: " + whiteWine.size());
+
+        Set<Double> uniqueValues = new HashSet<>();
+        uniqueValues.addAll(redWine);
+        uniqueValues.addAll(whiteWine);
+
+        List<Double> allWines = new ArrayList<>(uniqueValues);
+        System.out.println("Unique alcohol values across both datasets: " + allWines.size());
+
+    }
+    }
+
+
+
+
+
+
